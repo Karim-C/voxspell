6ed46d1dbbf6dee70867317d96cc7475d53b88d5
will <willjoemolloy@gmail.com>
2016-09-06 12:44:07 +1200

    update gitignore

--- Voxspell_prototype/src/voxspell/SpellingQuiz.java
+++ Voxspell_prototype/src/voxspell/SpellingQuiz.java
@@ -11,6 +11,17 @@ import javax.swing.JTextField;
 
 public class SpellingQuiz extends JPanel {
 
+	// Swing componenets
+	private JTextField _wordEntryField;
+	private JButton _enterWordBtn;
+	private JButton _newQuizBtn;
+	protected JTextArea _programOutputArea;
+	
+	private static int[] levels;
+	{
+		levels = { 1 };
+	}
+	
 	/**
 	 * Gets the file name from the sub type, then creates the GUI.
 	 */
@@ -19,14 +30,39 @@ public class SpellingQuiz extends JPanel {
 	}
 	
 	protected final void createGUI() {
-		this.setBorder(BorderFactory.createTitledBorder("Level ?"));
+		
+		
+	//	this.setBorder(BorderFactory.createTitledBorder("Level x"));
 		this.setPreferredSize(new Dimension(305,270));
+
+		// Area displayed by the program to the user
+		_programOutputArea = new JTextArea();
+		_programOutputArea.setEditable(false);
+		_programOutputArea.setPreferredSize(new Dimension(285,180));
+		this.add(_programOutputArea, BorderLayout.NORTH);
+		
+		// Where user enters the word
+		_wordEntryField = new JTextField();
+		_wordEntryField.setPreferredSize(new Dimension(210,25));
+		this.add(_wordEntryField, BorderLayout.EAST);
+
+		// Btn pressed by user after entering word
+		_enterWordBtn = new JButton("Enter");
+		this.add(_enterWordBtn, BorderLayout.WEST);
+
+		// New quiz button, only available after a quiz has completed
+		_newQuizBtn = new JButton("New Quiz");
+		this.add(_newQuizBtn, BorderLayout.EAST);
 
 	}
 	
 	public void newQuiz()  {
-		// TODO Auto-generated method stub
+		// popup asking for level
 		
+		
+		// 
+		int level = 10;
+		this.setBorder(BorderFactory.createTitledBorder("Level " + level));
 	}
 
 	
